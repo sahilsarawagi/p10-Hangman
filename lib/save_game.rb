93e05a0222210fc721c403694b7ffc5670decbfd
sahil sarawagi <sahilsarawagi1@gmail.com>
2023-12-02 14:10:07 +0530

    Add Save class for object serialization and saving to a file
    - Initialize Save class with an object and a save file name
    - Serialize the object using YAML.dump
    - Save the serialized object to a file in the 'saved_game' directory

--- lib/save_game.rb
+++ lib/save_game.rb
@@ -1,1 +1,21 @@
-# this will save (serialize) the current position of the game in yaml format
+require 'yaml'
+class Save
+  def initialize(object,save_file_name)
+    @obj = object
+    @save_file_name = save_file_name
+    serialize_object
+  end
+  def serialize_object
+    serializing_object = YAML.dump(@obj)
+    save_to_file(serializing_object)
+  end
+  def save_to_file(obj)
+    Dir.mkdir('saved_game') unless Dir.exist?('saved_game')
+    file_name= "saved_game/#{@save_file_name}.yaml"
+    File.open(file_name,'w') {|f| f.write(obj)}
+  end
+end
+
+
+
+
